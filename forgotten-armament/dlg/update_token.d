698cddafa3dfecc8f75144375abf43eda8f77b33
Mickaël <mleduque@gmail.com>
2022-05-06 21:30:22 +0200

    keep some boots on

--- forgotten-armament/dlg/update_token.d
+++ forgotten-armament/dlg/update_token.d
@@ -1,1 +1,1 @@
-REPLACE_ACTION_TEXT ~tokcre01~ ~GiveItemCreate("BOOT01",LastTalkedToBy,0,0,0)~ ~GiveItemCreate("MOBT04",LastTalkedToBy,0,0,0)~
+REPLACE_ACTION_TEXT ~tokcre01~ ~GiveItemCreate("BOOT01",LastTalkedToBy,0,0,0)~ ~GiveItemCreate("BOOT01",LastTalkedToBy,0,0,0)%LNL%GiveItemCreate("MOBT04",LastTalkedToBy,0,0,0)~
