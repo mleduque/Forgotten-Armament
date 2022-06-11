ca6497d44654e61dcedd4d3fd47c2ec20f39d9aa
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2022-06-10 22:37:57 -0700

    cespenar

--- forgotten-armament/dlg/moblk01.d
+++ forgotten-armament/dlg/moblk01.d
@@ -3,39 +3,30 @@ IF ~PartyHasItem("moblk01")~ THEN GOTO MO#PartyHasAtLeastOneItem END
   
 APPEND BOTSMITH
 IF ~~ THEN BEGIN MO#PartyHasAtLeastOneItem SAY @40000
+	IF ~PartyHasItem("moblk01")
+		PartyHasItem("compon04")~ THEN GOTO MO#PartyHasAllParts
+	IF ~PartyHasItem("moblk01")
+		!PartyHasItem("compon04")~ THEN GOTO MO#PartyDoesNotHaveAllParts
+END 
 
+IF ~~ THEN BEGIN MO#PartyDoesNotHaveAllParts SAY @40001
+	IF ~~ THEN GOTO MO#MovingRightAlong
+END 
 
-
-
-
-
-
-	IF ~~ THEN BEGIN MO#ItemUp00 SAY @567865
-		IF ~PartyHasItem("moblk01")
-			!PartyHasItem("compon04")~ THEN GOTO MO#ItemUp01
-		IF ~PartyHasItem("moblk01")
-			PartyHasItem("compon04")~ THEN GOTO MO#ItemUp02
-	END
-  
-	IF ~~ THEN BEGIN MO#ItemUp01 SAY @5678865
-		IF ~~ THEN GOTO MO#NoItemUp
-	END
-  
-	IF ~~ THEN BEGIN MO#ItemUp02 SAY @5678965
-		IF ~PartyGoldLT(10000)~ THEN REPLY #66662 GOTO MO#NoItemUp
-		IF ~PartyGoldGT(9999)~ THEN REPLY #66664 DO ~SetGlobal("MO#TOBItemUp","GLOBAL",1)
+IF ~~ THEN BEGIN MO#PartyHasAllParts SAY @40002
+	IF ~PartyGoldLT(10000)~ THEN REPLY #66662 GOTO MO#MovingRightAlong
+	IF ~PartyGoldGT(9999)~ THEN REPLY #66664 DO ~SetGlobal("MO#TOBItemUp","GLOBAL",1)
 											 		 SetGlobal("ImpForgeStuff","GLOBAL",1)
-													 SetGlobal("MO#TOBCraft","GLOBAL",1)
 													 TakePartyGold(10000)
 													 DestroyGold(10000)
 													 TakePartyItemNum("moblk01",1)
 													 DestroyItem("moblk01")
 													 TakePartyItemNum("compon04",1)
 													 DestroyItem("compon04")~ GOTO 11
-    IF ~~ THEN REPLY #66770 GOTO MO#NoItemUp
-	END
+	IF ~~ THEN REPLY #66770 GOTO MO#MovingRightAlong
+	END 
 
-	IF ~~ THEN BEGIN MO#NoItemUp SAY @5678
+	IF ~~ THEN BEGIN MO#MovingRightAlong SAY @40003
 		COPY_TRANS BOTSMITH 4
 	END
 END
