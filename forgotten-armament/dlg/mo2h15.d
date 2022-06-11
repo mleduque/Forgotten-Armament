aa564720ddefb39b0cd0184ba8554b71d8697337
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2022-06-10 23:30:21 -0700

    Update mo2h15.d

--- forgotten-armament/dlg/mo2h15.d
+++ forgotten-armament/dlg/mo2h15.d
@@ -15,7 +15,7 @@ END
 
 IF ~~ THEN BEGIN MO#PartyHasAllParts SAY @40012
 	IF ~PartyGoldLT(15000)~ THEN REPLY #66662 GOTO MO#MovingRightAlong
-	IF ~PartyGoldGT(14999)~ THEN REPLY #66664 DO ~SetGlobal("MO#TOBItemUp","GLOBAL",1)
+	IF ~PartyGoldGT(14999)~ THEN REPLY #66664 DO ~SetGlobal("MO#TOBItemUp","GLOBAL",2)
 											 		 SetGlobal("ImpForgeStuff","GLOBAL",1)
 													 TakePartyGold(15000)
 													 DestroyGold(15000)
