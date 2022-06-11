26494088aa483b7102e8c09f19e53b31e06a9fa7
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2022-06-10 23:28:43 -0700

    Cespenar

---
+++ forgotten-armament/dlg/mo2h15.d
@@ -0,0 +1,32 @@
+EXTEND_BOTTOM BOTSMITH 4
+IF ~PartyHasItem("mo2h15")~ THEN GOTO MO#PartyHasAtLeastOneItem END
+  
+APPEND BOTSMITH
+IF ~~ THEN BEGIN MO#PartyHasAtLeastOneItem SAY @40010
+	IF ~PartyHasItem("mo2h15")
+		PartyHasItem("compon01")~ THEN GOTO MO#PartyHasAllParts
+	IF ~PartyHasItem("mo2h15")
+		!PartyHasItem("compon01")~ THEN GOTO MO#PartyDoesNotHaveAllParts
+END 
+
+IF ~~ THEN BEGIN MO#PartyDoesNotHaveAllParts SAY @40011
+	IF ~~ THEN GOTO MO#MovingRightAlong
+END 
+
+IF ~~ THEN BEGIN MO#PartyHasAllParts SAY @40012
+	IF ~PartyGoldLT(15000)~ THEN REPLY #66662 GOTO MO#MovingRightAlong
+	IF ~PartyGoldGT(14999)~ THEN REPLY #66664 DO ~SetGlobal("MO#TOBItemUp","GLOBAL",1)
+											 		 SetGlobal("ImpForgeStuff","GLOBAL",1)
+													 TakePartyGold(15000)
+													 DestroyGold(15000)
+													 TakePartyItemNum("mo2h15",1)
+													 DestroyItem("mo2h15")
+													 TakePartyItemNum("compon01",1)
+													 DestroyItem("compon01")~ GOTO 11
+	IF ~~ THEN REPLY #66770 GOTO MO#MovingRightAlong
+	END 
+
+	IF ~~ THEN BEGIN MO#MovingRightAlong SAY @40013
+		COPY_TRANS BOTSMITH 4
+	END
+END
